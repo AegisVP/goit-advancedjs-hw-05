e58ce5f28f7d41e538093927f662b0fbd4f83d9f
Vlad P <vlad@pysarenko.com>
2024-06-22 21:59:01 +0200

    initial commit

---
+++ dst/index.js
@@ -0,0 +1,7 @@
+"use strict";
+const add = (a, b) => {
+    console.log('Passed: ', a, b);
+    return a + b;
+};
+console.log('Result: ', add(1, 2));
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+const add = (a: number, b: number) => {
+  console.log('Passed: ', a, b);
+  return a + b;
+};
+
+console.log('Result: ', add(1, 2));
